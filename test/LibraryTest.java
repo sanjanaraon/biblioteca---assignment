db8f085071986784d51beffcf617aa088252e234
sanjana rao n <sanjanar@thoughtworks.com>
2015-02-25 18:58:55 +0530

    started from sctrach with approach from domain objects

---
+++ test/LibraryTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * Created by sanjanar on 25/02/15.
+ */
+public class LibraryTest {
+    @Test
+    public void test() throws Exception {
+       // Mockito.mock();
+    }
+}
