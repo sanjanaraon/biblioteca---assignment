68c6cf94ce89fea3a0f71457a9e496633073061b
sanjana rao n <sanjanar@thoughtworks.com>
2015-02-25 15:47:31 +0530

    wrote tests for displaying welcome message and main menu

--- test/com/twu/biblioteca/BibliotecaTest.java
+++ test/com/twu/biblioteca/BibliotecaTest.java
@@ -1,14 +1,22 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class BibliotecaTest {
 
     @Test
     public void shouldDisplayWelcomeMessage() {
-        BibliotecaApp app=new BibliotecaApp();
-        
+        String expectedMessage="Namaste Welcome to Biblioteca Application \n" +
+                " The app to borrow book and return books";
+        Assert.assertEquals(expectedMessage,BibliotecaApp.displayWelcomeMessage());
+    }
+
+    @Test
+    public void shouldDisplayMainMenu() throws Exception {
+        String expectedMessage="Main Menu \nEnter your choice \n 1)List Books";
+        Assert.assertEquals(expectedMessage,BibliotecaApp.displayMainMenu());
     }
 }
