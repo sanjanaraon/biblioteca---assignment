59b6621f07975ec56b0603363c5a0b23687058ab
sanjana rao n <sanjanar@thoughtworks.com>
2015-02-26 09:45:28 +0530

    created a constructor to pass message related to the problem occured

--- src/com/biblioteca/InvalidBookException.java
+++ src/com/biblioteca/InvalidBookException.java
@@ -3,5 +3,8 @@ package com.biblioteca;
 /**
  * Created by sanjanar on 25/02/15.
  */
-public class InvalidBookException {
+public class InvalidBookException extends Exception {
+    public InvalidBookException() {
+        super("The Book is not available or It is not a Valid Book");
+    }
 }
