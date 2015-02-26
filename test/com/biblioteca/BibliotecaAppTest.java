e923e0849422738d870eb8463bc640e760949220
sanjana rao n <sanjanar@thoughtworks.com>
2015-02-26 17:54:50 +0530

    added a library

--- test/com/biblioteca/BibliotecaAppTest.java
+++ test/com/biblioteca/BibliotecaAppTest.java
@@ -1,6 +1,6 @@
 package com.biblioteca;
+
 import org.junit.Test;
-import org.mockito.Mockito;
 
 import java.util.List;
 
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 /**
  * Created by sanjanar on 26/02/15.
@@ -16,38 +15,38 @@ import static org.mockito.Mockito.when;
 public class BibliotecaAppTest {
     @Test
     public void shouldDisplayWelcomeMessage() throws Exception {
-        BibliotecaApp bibliotecaApp =new BibliotecaApp();
-        String expected= "Welcome to Biblioteca \n The app to borrow and return the books to the library";
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        String expected = "Welcome to Biblioteca \n The app to borrow and return the books to the library";
         assertEquals(expected, bibliotecaApp.displayMessage());
     }
 
     @Test
     public void shouldDisplayMainMenu() throws Exception {
-        BibliotecaApp bibliotecaApp =new BibliotecaApp();
-        String expected="Main menu \n 1 ---- Book Details \n 2 ---- Exit";
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        String expected = "Main menu \n 1 ---- Book Details \n 2 ---- Exit";
         assertEquals(expected, bibliotecaApp.displayMainMenu());
     }
 
     @Test
     public void shouldHaveAListOfBooksWhenTheAppIsInitialized() throws Exception {
-        BibliotecaApp bibliotecaApp=new BibliotecaApp();
-        Library library=new Library();
-        List<Book> expectedBooks=library.initializeBookList();
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        Library library = new Library();
+        List<Book> expectedBooks = library.initializeBookList();
         assertEquals(expectedBooks, bibliotecaApp.getBooks());
     }
 
     @Test
     public void shouldDisplayTheListOfBooksInTableForm() throws Exception {
-        BibliotecaApp bibliotecaApp=new BibliotecaApp();
-        String expected=bookListInTable(bibliotecaApp.getBooks());
-        assertEquals(expected,bibliotecaApp.displayBookDetails());
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        String expected = bookListInTable(bibliotecaApp.getBooks());
+        assertEquals(expected, bibliotecaApp.displayBookDetails());
     }
 
     private String bookListInTable(List<Book> books) {
-        String result=" ";
-        for (Book b:books){
-            if(b.isCheckedOut()==false)
-            result+= b.getTitle()+"    |"+b.getAuthor()+"   |"+b.getYear()+"\n";
+        String result = " ";
+        for (Book b : books) {
+            if (b.isCheckedOut() == false)
+                result += b.getTitle() + "    |" + b.getAuthor() + "   |" + b.getYear() + "\n";
         }
         return result;
     }
@@ -63,5 +62,44 @@ public class BibliotecaAppTest {
         bibliotecaApp.checkOutFromLibrary(book1);
 
         verify(mockLibrary).checkOut(book1);
+    }
+
+    @Test(expected = InvalidBookException.class)
+    public void shouldThrowExceptionWhenInvalidBookIsCheckedOut() throws Exception {
+        Book book1 = new Book("S C Q A D", "Kathy Serra", 2006);
+        Library mockLibrary = mock(Library.class);
+
+        doThrow(new InvalidBookException()).when(mockLibrary).checkOut(book1);
+
+        BibliotecaApp bibliotecaApp = new BibliotecaApp(mockLibrary);
+        bibliotecaApp.checkOutFromLibrary(book1);
+
+        verify(mockLibrary).checkOut(book1);
+    }
+
+    @Test
+    public void shouldReturnBookBackToLibrary() throws Exception {
+        Book book1 = new Book("S C J P", "Kathy Serra", 2006);
+        Library mockLibrary = mock(Library.class);
+
+        doNothing().when(mockLibrary).returnBook(book1);
+
+        BibliotecaApp bibliotecaApp = new BibliotecaApp(mockLibrary);
+        bibliotecaApp.returnBookFromLibrary(book1);
+
+        verify(mockLibrary).returnBook(book1);
+    }
+
+    @Test(expected = InvalidBookException.class)
+    public void shouldThrowExceptionWhenInvalidBookIsReturned() throws Exception {
+        Book book1 = new Book("S C J P", "Kathy Serra", 2006);
+        Library mocklibrary=mock(Library.class);
+
+        doThrow(new InvalidBookException()).when(mocklibrary).returnBook(book1);
+
+        BibliotecaApp bibliotecaApp=new BibliotecaApp(mocklibrary);
+        bibliotecaApp.returnBookFromLibrary(book1);
+
+        verify(mocklibrary).returnBook(book1);
     }
 }
