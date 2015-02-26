88f2b0de9bcfa037a7d55a1c476096c95212a893
sanjana rao n <sanjanar@thoughtworks.com>
2015-02-26 09:50:17 +0530

    refactored by creating a method for reseting the checkout which was common in checkout and return method

--- src/com/biblioteca/Library.java
+++ src/com/biblioteca/Library.java
@@ -1,7 +1,46 @@
 package com.biblioteca;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by sanjanar on 25/02/15.
  */
 public class Library {
+    List<Book> books = new ArrayList<Book>();
+
+    public Library() {
+
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void checkOut(Book book) throws InvalidBookException {
+        if (books.contains(book) && book.isCheckedOut() == false)
+            resetCheckOut(book, true);
+        else
+            throw new InvalidBookException();
+
+    }
+
+    public void returnBook(Book book) throws InvalidBookException {
+        if (books.contains(book) && book.isCheckedOut() == true)
+            resetCheckOut(book, false);
+        else
+            throw new InvalidBookException();
+    }
+
+    private void resetCheckOut(Book book, boolean flag) {
+        for (Book b : books) {
+            if (b == book) {
+                b.setCheckedOut(flag);
+            }
+        }
+    }
 }
