5917acf88fd59d0d6bda06554de91e55dce021bb
sanjana rao n <sanjanar@thoughtworks.com>
2015-02-26 16:32:50 +0530

    added the getter methods again

--- src/com/biblioteca/Book.java
+++ src/com/biblioteca/Book.java
@@ -17,17 +17,17 @@ public class Book {
     }
 
 
-//    public String getTitle() {
-//        return title;
-//    }
-//
-//    public String getAuthor() {
-//        return author;
-//    }
-//
-//    public int getYear() {
-//        return year;
-//    }
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
 
     public void setCheckedOut(boolean checkedOut) {
         this.checkedOut = checkedOut;
