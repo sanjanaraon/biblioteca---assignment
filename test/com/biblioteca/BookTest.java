2506989d56b6fbf64d1601c621c6e2d4ec94cf30
sanjana rao n <sanjanar@thoughtworks.com>
2015-02-26 16:39:20 +0530

    tested the equals method

--- test/com/biblioteca/BookTest.java
+++ test/com/biblioteca/BookTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by sanjanar on 25/02/15.
@@ -13,5 +15,19 @@ public class BookTest {
     public void shouldPrintBookDetails() throws Exception {
         Book book=new Book("S C J P","Kathy Serra",2006);
         assertEquals(new Book("S C J P","Kathy Serra",2006).toString() ,book.toString());
+    }
+
+    @Test
+    public void compare2BooksAndReturnTrue() throws Exception {
+        Book book1=new Book("S C J P","Kathy Serra",2006);
+        Book book2=new Book("S C J P","Kathy Serra",2006);
+        assertTrue(book1.equals(book2));
+    }
+
+    @Test
+    public void compare2BooksAndReturnFalse() throws Exception {
+        Book book1=new Book("S C J P","Kathy Serra",2006);
+        Book book2=new Book("Let us C", "Yeshwanth", 2000);
+        assertFalse(book1.equals(book2));
     }
 }
