3c9f8ac6f90c90077755c20efd2c9ffb2c3aa2b8
sanjana rao n <sanjanar@thoughtworks.com>
2015-02-26 17:56:53 +0530

    created a test class for testing the AppStarter class

---
+++ test/com/biblioteca/AppStarterTest.java
@@ -0,0 +1,17 @@
+package com.biblioteca;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by sanjanar on 26/02/15.
+ */
+public class AppStarterTest {
+    @Test
+    public void shouldDisplayWelcomeMessage() throws Exception {
+        AppStarter appStarter =new AppStarter();
+
+    }
+}
