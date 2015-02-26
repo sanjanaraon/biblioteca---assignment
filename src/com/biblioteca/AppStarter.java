b422b713ab168739e9bfd421ee4b05f9210db54f
sanjana rao n <sanjanar@thoughtworks.com>
2015-02-26 18:06:46 +0530

    created a class for interacting with user

---
+++ src/com/biblioteca/AppStarter.java
@@ -0,0 +1,12 @@
+package com.biblioteca;
+
+/**
+ * Created by sanjanar on 26/02/15.
+ */
+public class AppStarter {
+    public static void main(String[] args) {
+        BibliotecaApp bibliotecaApp=new BibliotecaApp();
+        bibliotecaApp.displayMessage();
+    }
+
+}
