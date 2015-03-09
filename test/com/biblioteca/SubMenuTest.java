7a88e80491d43b81d799c2a8d843bfe21ef49264
sanjana rao n <sanjanar@thoughtworks.com>
2015-03-09 11:03:13 +0530

    added a condition for printing message when invalid choice was entered by user

--- test/com/biblioteca/SubMenuTest.java
+++ test/com/biblioteca/SubMenuTest.java
@@ -2,6 +2,7 @@ package com.biblioteca;
 
 import com.biblioteca.core.models.Book;
 import com.biblioteca.core.models.Item;
+import com.biblioteca.core.models.UserInfo;
 import com.biblioteca.core.security.AccountManager;
 import com.biblioteca.ui.controller.LibraryManager;
 import com.biblioteca.ui.menuaction.SubMenu;
@@ -43,7 +44,7 @@ public class SubMenuTest {
         when(bookLibraryManager.displayMainMenu()).thenReturn("Main menu ");
         when(bookLibraryManager.displayItemDetails(list)).thenReturn("Book Details ");
         testReaderWriter.consoleInput("1\n4");
-        String expected = "Main menu Items available for borrowingBook Details Main menu Enter a valid choiceSuccessful Exit from Menu";
+        String expected = "Main menu Items available for borrowingBook Details Main menu Successful Exit from Menu";
        subMenu.menu(list, bookLibraryManager, testReaderWriter, manager);
         assertEquals(expected, testReaderWriter.consoleOutput());
 
@@ -53,18 +54,19 @@ public class SubMenuTest {
     public void shouldCallCheckOutBookMenuActionWhen2IsPassedWithValidCredentials() throws Exception {
         Book book = new Book("S C J P", "Kathy Serra", 2006);
 
-        bookLibraryManager = mock(LibraryManager.class);
-        // Item newBook=bookLibraryManager.getItem("s c j p");
-        subMenu =new SubMenu(testReaderWriter, bookLibraryManager);
+       bookLibraryManager = mock(LibraryManager.class);
+        Item newBook=bookLibraryManager.getItem("s c j p");
+       subMenu =new SubMenu(testReaderWriter, bookLibraryManager);
+       UserInfo user=new UserInfo("lib-1000","user1","anu","anu@ymail.com","8921679023","user");
 
-        when(bookLibraryManager.displayMainMenu()).thenReturn("Main menu ");
+       when(bookLibraryManager.displayMainMenu()).thenReturn("Main menu ");
         when(bookLibraryManager.displaySpecificItemListDetails(bookLibraryManager.getItemListByCategory("book"))).thenReturn("Book Details ");
         when(bookLibraryManager.validTitle("S C J P")).thenReturn(true);
         when(bookLibraryManager.getItem("S C J P")).thenReturn(book);
-        doNothing().when(bookLibraryManager).checkOutFromLibrary(book);
-        testReaderWriter.consoleInput("2\nlib-1001\nuser2\nS C J P\n4");
+        doNothing().when(bookLibraryManager).checkOutFromLibrary(book, user);
+        testReaderWriter.consoleInput("2\nlib-1000\nuser1\nS C J P\n4");
 
-        String expected = "Main menu Enter library numberEnter passwordBook Details Select a Item by entering the titleS C J P is checked out successfullyMain menu Enter a valid choiceSuccessful Exit from Menu";
+        String expected = "Main menu Enter library numberEnter passwordBook Details Select a Item by entering the titleS C J P is checked out successfullyMain menu Successful Exit from Menu";
 
         subMenu.menu(bookLibraryManager.getItemListByCategory("book"), bookLibraryManager,testReaderWriter, manager);
 
@@ -83,7 +85,7 @@ public class SubMenuTest {
         when(bookLibraryManager.validTitle("S C J P")).thenReturn(true);
         testReaderWriter.consoleInput("2\nlib-1001\nasds\n4");
 
-        String expected = "Main menu Enter library numberEnter passwordNot a valid userMain menu Enter a valid choiceSuccessful Exit from Menu";
+        String expected = "Main menu Enter library numberEnter passwordNot a valid userMain menu Successful Exit from Menu";
 
         subMenu.menu(list, bookLibraryManager,testReaderWriter, manager);
 
@@ -95,6 +97,7 @@ public class SubMenuTest {
     public void shouldCallReturnBookMenuActionWhen3IsPassedWithValidCredentials() throws Exception {
         bookLibraryManager = mock(LibraryManager.class);
         List<Item> list= bookLibraryManager.getItemListByCategory("book");
+        UserInfo user=new UserInfo("lib-1000","user1","anu","anu@ymail.com","8921679023","user");
 
         subMenu =new SubMenu(testReaderWriter, bookLibraryManager);
 
@@ -102,9 +105,9 @@ public class SubMenuTest {
         Book book1 = new Book("S C J P", "Kathy Serra", 2006);
         when(bookLibraryManager.getItem("S C J P")).thenReturn(book1);
         when(bookLibraryManager.displayItemDetailsInTableForm(list)).thenReturn("books");
-        doNothing().when(bookLibraryManager).returnBookToLibrary(book1);
+        doNothing().when(bookLibraryManager).returnBookToLibrary(book1, user);
         testReaderWriter.consoleInput("3\nlib-1000\nuser1\nS C J P\n4");
-        String expected = "main menuEnter library numberEnter passwordbooksSelect a Item by entering the titleS C J P returned to the librarymain menuEnter a valid choiceSuccessful Exit from Menu";
+        String expected = "main menuEnter library numberEnter passwordbooksSelect a Item by entering the titleS C J P returned to the librarymain menuSuccessful Exit from Menu";
         subMenu.menu(list, bookLibraryManager,testReaderWriter, manager);
         assertEquals(expected,testReaderWriter.consoleOutput());
 
@@ -116,14 +119,15 @@ public class SubMenuTest {
         List<Item> list= bookLibraryManager.getItemListByCategory("book");
 
         subMenu =new SubMenu(testReaderWriter, bookLibraryManager);
+        UserInfo user=new UserInfo("lib-1000","user1","anu","anu@ymail.com","8921679023","user");
 
         when(bookLibraryManager.displayMainMenu()).thenReturn("main menu");
         Book book1 = new Book("S C J P", "Kathy Serra", 2006);
         when(bookLibraryManager.getItem("S C J P")).thenReturn(book1);
         when(bookLibraryManager.displaySpecificItemListDetails(list)).thenReturn("books");
-        doNothing().when(bookLibraryManager).returnBookToLibrary(book1);
+        doNothing().when(bookLibraryManager).returnBookToLibrary(book1, user);
         testReaderWriter.consoleInput("3\nlib-1000\nuasd\n4");
-        String expected = "main menuEnter library numberEnter passwordNot a valid usermain menuEnter a valid choiceSuccessful Exit from Menu";
+        String expected = "main menuEnter library numberEnter passwordNot a valid usermain menuSuccessful Exit from Menu";
         subMenu.menu(list, bookLibraryManager,testReaderWriter, manager);
         assertEquals(expected,testReaderWriter.consoleOutput());
 
@@ -141,6 +145,6 @@ public class SubMenuTest {
                 " 2 ---- Check Out a Item\n" +
                 " 3 ---- Return a Item\n" +
                 " 4 ---- Exit\n" +
-                "Enter your choiceEnter a valid choiceSuccessful Exit from Menu",testReaderWriter.consoleOutput());
+                "Enter your choiceSuccessful Exit from Menu",testReaderWriter.consoleOutput());
     }
 }
